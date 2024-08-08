9dd55bf106d750c4e179df1766aac2204bc927c5
Olga B <olga.budko.99@list.ru>
2024-08-08 14:40:53 +0300

    лекция на повторение-ДЗ

--- addressbook_web_tests/src/test/java/GroupCreatingTests.java
+++ addressbook_web_tests/src/test/java/GroupCreatingTests.java
@@ -1,14 +1,10 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.HashMap;
-import java.util.NoSuchElementException;
 
 public class GroupCreatingTests {
     private WebDriver driver;
@@ -62,7 +58,10 @@ public class GroupCreatingTests {
     @Test
     public void canCreatedGroupWithEmptyName() {
 
-        driver.findElement(By.linkText("groups")).click();
+        if (! isElementPresent(By.name("new"))) {
+
+            driver.findElement(By.linkText("groups")).click();
+        }
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).sendKeys("");
