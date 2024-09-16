0cde3f2c4a58e0178be27eefdd1dc4660a5329ca
Olga B <olga.budko.99@list.ru>
2024-09-16 11:13:03 +0300

    добавление конфигурации

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -4,18 +4,23 @@ import manage.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.Random;
 
 public class TestBase {
     protected static ApplicationManager app;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         if (app==null){
+            var properties=new Properties();
+            properties.load(new FileReader(System.getProperty("target","local.properties")));
             app=new ApplicationManager();
-        }
-        app.init(System.getProperty("browser","firefox"));
+
+        app.init(System.getProperty("browser","firefox"), properties);}
     }
 
     public static String randomFile(String dir){
