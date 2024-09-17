b1daa2f2b8299ac5f3cd41227b4fbe44dbf5e5f4
Olga B <olga.budko.99@list.ru>
2024-09-17 10:32:22 +0300

    удаление групп через БД

--- addressbook_web_tests/src/test/java/manage/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manage/HibernateHelper.java
@@ -35,6 +35,14 @@ public class HibernateHelper extends HelperBase{
         return new GroupData("" + record.id, record.name, record.header, record.footer);
     }
 
+    private static GroupRecord convert(GroupData data) {
+        var id=data.id();
+        if ("".equals(id)){
+            id="0";
+        }
+        return new GroupRecord(Integer.parseInt(id), data.name(), data.header(), data.footer());
+    }
+
     public List<GroupData> getGroupList() {
         return convertList(sessionFactory.fromSession(session -> {
             return session.createQuery("from GroupRecord", GroupRecord.class).list();
@@ -42,4 +50,18 @@ public class HibernateHelper extends HelperBase{
     }
 
 
+    public long getGroupCount() {
+        return sessionFactory.fromSession(session -> {
+            return session.createQuery("select count(*) from GroupRecord", Long.class).getSingleResult();
+        });
+    }
+
+    public void createdGroup(GroupData groupData) {
+        sessionFactory.inSession(session -> {
+            session.getTransaction().begin();
+            session.persist(convert(groupData));
+            session.getTransaction().commit();
+
+        });
+    }
 }
--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -39,5 +39,35 @@ public class GroupRemovalTests extends TestBase {
 
     }
 
+    @Test
+    public void canRemoveGroupFromDB() {
+
+        if (app.hbm().getGroupCount()==0){
+            app.hbm().createdGroup(new GroupData("", "", "", ""));
+        }
+        //int groupCount=app.groups().getCount();
+        var oldGroups=app.hbm().getGroupList();
+        var rnd=new Random();
+        var index=rnd.nextInt(oldGroups.size());
+
+        app.groups().removeGroup(oldGroups.get(index));
+        var newGroups=app.hbm().getGroupList();
+        var expectedList=new ArrayList<>(oldGroups);
+        expectedList.remove(index);
+
+        //int newGroupCount=app.groups().getCount();
+        Assertions.assertEquals(newGroups,expectedList);
+    }
+
+    @Test
+    void canRemovalAllGroupsAtOnceFromDB(){
+        if (app.hbm().getGroupCount()==0){
+            app.hbm().createdGroup(new GroupData("", "", "", ""));
+        }
+        app.groups().removeAllGroups();
+        Assertions.assertEquals(0,app.hbm().getGroupCount());
+
+    }
+
 
 }
